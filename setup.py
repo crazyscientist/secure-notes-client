38897428d596f5bd53e1e2ecfd19b9e31d5d0737
Andreas Hasenkopf <andi@hasenkopf2000.net>
2019-01-06 16:05:24 +0100

    Updated docs

--- setup.py
+++ setup.py
@@ -2,6 +2,8 @@
 
 from setuptools import setup, find_packages
 
+with open("README.md") as fh:
+    long_description = fh.read()
 
 setup(
     name='securenotes-client',
@@ -9,9 +11,26 @@ setup(
     author="Andreas Hasenkopf",
     author_email="andreas@hasenkopf.xyz",
     description="Command line client for SecureNotes",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url="https://github.com/crazyscientist/secure-notes-client",
     packages=find_packages(),
     python_requires=">=3.5",
-    scripts=["securenotes.py",],
-    install_requires=['requests', 'pycryptodomex', 'mdv']
+    scripts=["securenotes_client/securenotes.py",],
+    install_requires=['requests', 'pycryptodomex', 'mdv'],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Console",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    project_urls={
+        "Bug Tracker": "https://github.com/crazyscientist/secure-notes-client/issues",
+        "Documentation": "https://readthedocs.org/projects/secure-notes-client/",
+        "Source Code": "https://github.com/crazyscientist/secure-notes-client"
+    }
 )
